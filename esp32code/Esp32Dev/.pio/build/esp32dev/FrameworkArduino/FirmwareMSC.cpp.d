f03b0cb17704bd5d6f97827ded358d2b9481ab88
--global <--global>
2026-08-27 22:32:54 +0800

    add esp32 config

---
+++ esp32code/Esp32Dev/.pio/build/esp32dev/FrameworkArduino/FirmwareMSC.cpp.d
@@ -0,0 +1,5 @@
+.pio/build/esp32dev/FrameworkArduino/FirmwareMSC.cpp.o: \
+ /home/rayfei/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.cpp \
+ /home/rayfei/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.h \
+ /home/rayfei/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
+ /home/rayfei/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
